f27fb9f0873754e59e792cc6eb2d4031ad45b7d3
testeProgramador21 <146689338+testeProgramador21@users.noreply.github.com>
2023-10-02 00:14:55 -0300

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+//ANIMAÇÕES
+
+const Page = () => {
+  return (
+    <div className="container mx-auto">
+      <div className="transition-all duration-200 ease-linear w-40 h-24 bg-green-500  hover:w-full"></div>
+
+    </div>
+  );
+}
+
+export default Page;
